b566ce5984cecef7e64902f10cae6ca41ac2a56f
mogan <moganes@yahoo.com>
2026-07-06 08:10:10 -0600

    perf: add @@index([orgId]) to org-scoped models

---
+++ prisma/migrations/20260706140916_add_org_indexes/migration.sql
@@ -0,0 +1,17 @@
+-- CreateIndex
+CREATE INDEX "GamePlan_orgId_idx" ON "GamePlan"("orgId");
+
+-- CreateIndex
+CREATE INDEX "Membership_orgId_idx" ON "Membership"("orgId");
+
+-- CreateIndex
+CREATE INDEX "Playbook_orgId_idx" ON "Playbook"("orgId");
+
+-- CreateIndex
+CREATE INDEX "PracticePlan_orgId_idx" ON "PracticePlan"("orgId");
+
+-- CreateIndex
+CREATE INDEX "Quiz_orgId_idx" ON "Quiz"("orgId");
+
+-- CreateIndex
+CREATE INDEX "TeamFile_orgId_idx" ON "TeamFile"("orgId");
